03d9dd169be7e117266dc32298e15a35892d1b28
Alisa <alisamun2006@gmail.com>
2022-12-14 07:19:41 +1000

    homework 4

---
+++ 4. abstraction and encapsulation/1/1/1.cpp
@@ -0,0 +1,55 @@
+﻿// В этом задании вы будете читать адреса из файла и выводить их в другой файл в другом формате.
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+class Address {
+public:
+	int N;
+	int stock;
+	std::string sstock;
+
+	Address() {
+		std::ifstream file("in.txt");
+		if (file.is_open()) {
+			file >> N;;
+			file.close();
+		}
+	}
+
+	void newFile() {
+		std::ofstream newfile("out.txt", std::ios::app);
+		if (newfile.is_open()) {
+			newfile << N << std::endl;
+		}
+		for (int i = N - 1; i >= 0; --i) {
+			std::ifstream file("in.txt");
+			if (file.is_open() && newfile.is_open()) {
+				file >> stock;
+				for (int j = 0; j < i; ++j) {
+					file >> sstock;
+					file >> sstock;
+					file >> stock;
+					file >> stock;
+				}
+				file >> sstock;
+				newfile << sstock << ", ";
+				file >> sstock;
+				newfile << sstock << ", ";
+				file >> stock;
+				newfile << stock << ", ";
+				file >> stock;
+				newfile << stock << std::endl;
+				file.close();
+			}
+		}
+		newfile.close();
+	}
+
+};
+
+int main() {
+	Address ad;
+	ad.newFile();
+}
---
+++ 4. abstraction and encapsulation/2/2/2.cpp
@@ -0,0 +1,96 @@
+﻿
+
+//В этом задании вы будете читать из файла адреса и выводить их в другой файл в другом формате, отсортированными по названию города.
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+enum Letters {
+	A = 1,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J,
+	K,
+	L,
+	M,
+	N,
+	O,
+	P,
+	Q,
+	R,
+	S,
+	T,
+	U,
+	V,
+	W,
+	X,
+	Y,
+	Z
+};
+
+void func() {
+	int N;
+	std::ifstream in("in.txt");
+	if (in.is_open()) {
+		in >> N;
+		std::ofstream out("out.txt");
+		if (out.is_open()) {
+			out << N << std::endl;
+			std::string *City = new std::string[N];
+			std::string *Street = new std::string[N];
+			int *House = new int[N];
+			int *Flat = new int[N];
+			for (int i = 0; i < N; ++i) {
+				in >> City[i];
+				in >> Street[i];
+				in >> House[i];
+				in >> Flat[i];
+			}
+			in.close();
+			int* numbers = new int[N];
+			for (int i = 0; i < N; ++i) {
+				std::string s = City[i];
+				char c = s[0];
+				Letters l = static_cast<Letters>(c);
+				numbers[i] = l;
+			}
+
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N - 1; ++j) {
+					if (numbers[j] > numbers[j + 1]) {
+						std::swap(City[j], City[j + 1]);
+						std::swap(House[j], House[j + 1]);
+						std::swap(Street[j], Street[j + 1]);
+						std::swap(Flat[j], Flat[j + 1]);
+						std::swap(numbers[j], numbers[j + 1]);
+					}
+				}
+			}
+
+			for (int i = 0; i < N; ++i) {
+				out << City[i] << ", ";
+				out << Street[i] << ", ";
+				out << House[i] << ", ";
+				out << Flat[i] << std::endl;
+			}
+			delete[] numbers;
+			out.close();
+			delete[] City;
+			delete[] Street;
+			delete[] House;
+			delete[] Flat;
+		}
+	}
+}
+
+
+int main() {
+	func();
+}
