f3f4b7219b1a74cd6590a69b75e9a6b5d9daa7c5
Alisa <alisamun2006@gmail.com>
2022-12-29 14:15:02 +1000

    hm

---
+++ 8. Exceptions and error handling/2/2/Source.cpp
@@ -0,0 +1,88 @@
+class ext {
+public:
+
+	class NOTZEROSIDE {
+	public:
+		const char* notzeroside() {
+			return "sides aren't 0";
+		}
+	};
+
+	class NOTTHREESIDE {
+	public:
+		const char* notthreeside() {
+			return "sides aren't 3";
+		}
+	};
+
+	class WRONGTRIANGLESUM {
+	public:
+		const char* wrongtrianglesum() {
+			return "angles' sum isn't 180";
+		}
+	};
+
+	class NORIGHTANGLE {
+	public:
+		const char* norightangle() {
+			return "ahgle C isn't 90";
+		}
+	};
+
+	class NOTISOSCELES {
+	public:
+		const char* notisosceles() {
+			return "not an isosceles";
+		}
+	};
+
+	class NOTEQUILATERAL {
+	public:
+		const char* notequilateral() {
+			return "not an equilateral";
+		}
+	};
+
+	class NOTFOURSIDE {
+	public:
+		const char* notfourside() {
+			return "sides aren't 4";
+		}
+	};
+
+	class WRONGQADRILATERALSUM {
+	public:
+		const char* wrongqadrilateralsum() {
+			return "angles' sum isn't 360";
+		}
+	};
+
+	class NOTRECTANGLE {
+	public:
+		const char* notrectangle() {
+			return "not a rectangle";
+		}
+	};
+
+	class NOTSQUARE {
+	public:
+		const char* notsquare() {
+			return "nor a square";
+		}
+	};
+
+	class NOTPARALLELOGRAM {
+	public:
+		const char* notparallelogram() {
+			return "not a parallelogram";
+		}
+	};
+
+	class NOTRHOMBUS {
+	public:
+		const char* notrhombus() {
+			return "not a rhombus";
+		}
+	};
+
+};
